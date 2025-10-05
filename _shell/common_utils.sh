f39f304a1053e815169ed5dcdbcaf6b248757c3d
leetangle <adszzz11@gmail.com>
2025-10-05 20:02:22 +0900

    디렉터리 현행화

--- _shell/common_utils.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Legacy wrapper for common_utils functionality
-# This file maintains backward compatibility for scripts expecting _shell/common_utils.sh
-
-# Get the directory of this script
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Source the new modular common_utils_main.sh
-source "${SCRIPT_DIR}/common_utils/common_utils_main.sh"
